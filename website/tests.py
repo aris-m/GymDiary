82fe8dfe1cd4dcccd3f012cc8d4e9a3740917ef8
aris-m <aris.munandar3141@gmail.com>
2024-03-18 16:14:00 +0100

    add view index page test

--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class tests(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+        self.index_url = reverse('index')
+        self.login_url = reverse('login')
+        self.logout_url = reverse('logout')
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_index_view(self):
+        response = self.client.get(self.index_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
