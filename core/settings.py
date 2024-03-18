64b0a660c4cb7801fd54c57c927b11d6d1615c6f
aris-m <aris.munandar3141@gmail.com>
2024-03-18 16:49:51 +0100

    update build.yml

--- core/settings.py
+++ core/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "NAME": "GymDiary",
         "USER": "root",
         "PASSWORD": "Password#123",
-        "HOST": "localhost",
+        "HOST": "127.0.0.1",
         "PORT": "3306",
     }
 }
