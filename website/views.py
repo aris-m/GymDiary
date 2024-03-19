bdf98631ae74d14276b8423286bb3bc610524125
aris-m <aris.munandar3141@gmail.com>
2024-03-19 14:51:21 +0100

    update messages display

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.messages import constants
 from .forms import SignUpForm
 
 def index(request):
@@ -14,10 +15,10 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, "You have been logged in.")
+            messages.success(request, "You have been logged in.", extra_tags="success")
             return redirect("index")
         else:
-            messages.error(request, "There was an error while trying to log in, please try again")
+            messages.error(request, "There was an error while trying to log in, please try again", extra_tags="error")
             return redirect("login")
     
     return render(request, "login.html", {})
@@ -28,16 +29,19 @@ def logout_user(request):
     return redirect("index")
 
 def register_user(request):
-	if request.method == 'POST':
-		form = SignUpForm(request.POST)
-		if form.is_valid():
-			form.save()
-			username = form.cleaned_data['username']
-			password = form.cleaned_data['password1']
-			user = authenticate(username=username, password=password)
-			login(request, user)
-			messages.success(request, "You Have Successfully Registered! Welcome!")
-			return redirect('index')
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome!", extra_tags="success")
+            return redirect('index')
+        else:
+            messages.error(request, "Registration failed, please try again", extra_tags="error")
+            return redirect('register')
+    form = SignUpForm()
+    return render(request, 'register.html', {'form':form})
 	
-	form = SignUpForm()
-	return render(request, 'register.html', {'form':form})
