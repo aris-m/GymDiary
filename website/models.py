1424e62d7cfd30491aac8bcb528a741c370eaa2d
aris-m <aris.munandar3141@gmail.com>
2024-03-20 09:17:57 +0100

    add WorkoutSession and Goal models

--- website/models.py
+++ website/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Workout(models.Model):
-    registered_at = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=50)
     type = models.CharField(max_length=50)
     muscle_groups = models.CharField(max_length=100)
+
+class WorkoutSession(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    workouts = models.ManyToManyField(Workout)
+    date = models.DateTimeField(auto_now_add=True)
+    duration = models.DurationField()
+    
+class Goal(models.Model):
+    workout = models.OneToOneField(Workout, on_delete=models.CASCADE, null=True, blank=True)
+    sets = models.IntegerField()
+    reps = models.IntegerField()
+    weight = models.DecimalField(max_digits=6, decimal_places=2)
