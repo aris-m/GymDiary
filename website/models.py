121676470d69a779286ab8d8ba534514ad87abc3
aris-m <aris.munandar3141@gmail.com>
2024-03-21 13:14:59 +0100

    update models relationships

--- website/models.py
+++ website/models.py
@@ -2,19 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Workout(models.Model):
+    workout_session = models.ForeignKey('WorkoutSession', on_delete=models.CASCADE, related_name='session_workouts', default=None)
     name = models.CharField(max_length=50)
     type = models.CharField(max_length=50)
     muscle_groups = models.CharField(max_length=100)
 
-class WorkoutSession(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    workouts = models.ManyToManyField(Workout)
-    date = models.DateField()
-    duration = models.IntegerField(null=True, blank=True)
-    notes = models.TextField(max_length=200, null=True, blank=True)
-    
 class Goal(models.Model):
-    workout = models.OneToOneField(Workout, on_delete=models.CASCADE, null=True, blank=True)
+    workout_session = models.ForeignKey('WorkoutSession', on_delete=models.CASCADE, related_name='session_goals', default=None)
     sets = models.IntegerField()
     reps = models.IntegerField()
     weight = models.DecimalField(max_digits=6, decimal_places=2)
+
+class WorkoutSession(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    workouts = models.ManyToManyField(Workout, related_name='sessions')
+    goals = models.ManyToManyField(Goal, related_name='sessions')
+    date = models.DateField()
+    duration = models.IntegerField(null=True, blank=True)
+    notes = models.TextField(max_length=200, null=True, blank=True)
