7130d0f9344a768ce669d3cf0de78ca9d6e2a2b9
aris-m <aris.munandar3141@gmail.com>
2026-01-19 10:25:26 +0100

    Fix: automated test

--- core/settings_test.py
+++ core/settings_test.py
@@ -77,13 +77,9 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'GymDiary',
-        'USER': 'root',
-        'PASSWORD': 'Password#123',
-        'HOST': '127.0.0.1',
-        'PORT': '3306', 
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
--- website/urls.py
+++ website/urls.py
@@ -32,5 +32,4 @@ urlpatterns = [
     path('add-friend/<int:friend_id>/', views.add_friend, name='add-friend'),
     path('search-friends/', views.search_friends, name='search-friends'),
     path('unfriend/<int:friend_id>/', views.unfriend, name='unfriend'),
-    path('health/', views.health_check, name='health-check'),
 ]
